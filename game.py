af984ee9c3287938452b5de90e3423a724e8a2fb
Juan José <correopersonal436@gmail.com>
2022-02-21 09:37:05 +0100

    Fichero main

--- game.py
+++ game.py
@@ -1,41 +1,46 @@
 from entidades import Tablero, Barco, Disparo
 from __init__ import *
 
-# Creo los tableros
-usuario = Tablero().tablero()
-maquina = Tablero().tablero()
+class Game():
+    def __init__(self):
+        pass
 
-usuario_visible = Tablero().tablero()
-maquina_visible = Tablero().tablero()
-# Instancio los barcos de cada tablero y la lista de barcos de cada jugador
-Barco().barco(usuario, BARCOS)
-Barco().barco(maquina, BARCOS)
+    def partida(self):
+        # Creo los tableros
+        usuario = Tablero().tablero()
+        maquina = Tablero().tablero()
 
-print("Usuario", usuario_visible)
-print("\t")
-print("Máquina", maquina_visible)
+        usuario_visible = Tablero().tablero()
+        maquina_visible = Tablero().tablero()
+        # Instancio los barcos de cada tablero y la lista de barcos de cada jugador
+        Barco().barco(usuario, BARCOS)
+        Barco().barco(maquina, BARCOS)
 
-# Creo la forma de ruptura del bucle
-dic_usuario = {"X" : 0}
-dic_maquina = {"X" : 0}
+        print("Usuario", usuario_visible)
+        print("\t")
+        print("Máquina", maquina_visible)
 
-while True:
-    for i in usuario_visible:
-        for j in i:
-            if j == "X":
-                dic_usuario["X"] += 1
-    for i in maquina_visible:
-        for j in i:
-            if j == "X":
-                dic_maquina["X"] += 1
-    if dic_usuario["X"] == 20:
-        print("Ha ganado la máquina!")
-        break
-    elif dic_maquina["X"] == 20:
-        print("¡Enhorabuena! Ganaste")
-        break
-    else:
-            while Disparo().disparo_usuario(tablero_invisible=maquina, tablero_visible=maquina_visible) == True:
-                print(maquina_visible)
-            while Disparo().disparo_maquina(tablero_invisible=usuario, tablero_visible=usuario_visible) == True:
-                print(usuario_visible)
+        # Creo la forma de ruptura del bucle
+        dic_usuario = {"X" : 0}
+        dic_maquina = {"X" : 0}
+
+        while True:
+            for i in usuario_visible:
+                for j in i:
+                    if j == "X":
+                        dic_usuario["X"] += 1
+            for i in maquina_visible:
+                for j in i:
+                    if j == "X":
+                        dic_maquina["X"] += 1
+            if dic_usuario["X"] == 20:
+                print("Ha ganado la máquina!")
+                break
+            elif dic_maquina["X"] == 20:
+                print("¡Enhorabuena! Ganaste")
+                break
+            else:
+                    while Disparo().disparo_usuario(tablero_invisible=maquina, tablero_visible=maquina_visible) == True:
+                        print(maquina_visible)
+                    while Disparo().disparo_maquina(tablero_invisible=usuario, tablero_visible=usuario_visible) == True:
+                        print(usuario_visible)
