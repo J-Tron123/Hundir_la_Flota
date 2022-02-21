b38439b091a5a4237abefed06caa8774e7446dec
Juan José <correopersonal436@gmail.com>
2022-02-21 10:21:28 +0100

    Lista

--- __init__.py
+++ __init__.py
@@ -6,5 +6,5 @@ el_de_4 = [4, 7, 4, 6]
 el_de_3 = [3, 8, 3, 7]
 el_de_2 = [2, 9, 2, 8]
 el_de_1 = [1, 9, 0, 8]
-BARCOS = [el_de_4, el_de_3, el_de_3, el_de_2, el_de_2, el_de_2, el_de_1, el_de_1, el_de_1, el_de_1 ]
+BARCOS = [el_de_4, el_de_3, el_de_3, el_de_2, el_de_2, el_de_2, el_de_1, el_de_1, el_de_1, el_de_1]
 
